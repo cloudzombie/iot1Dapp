fcccd8b3ab81e7a9fff78779e5a516a82617b187
Max Kordek <max@lisk.io>
2016-02-16 21:50:31 +0100

    f

--- modules/contracts/HomeFunction.js
+++ modules/contracts/HomeFunction.js
@@ -8,85 +8,85 @@ function HomeFunction(cb, _library) {
 	cb(null, self);
 }
 
-HomeFunction.prototype.create = function (data, trs2) {
-	trs2.recipientId = data.recipientId;
-	trs2.asset2 = {
-		accountId: new Buffer(data.accountId, 'utf8').toString('hex'), // Save as hex string
-		deviceId: new Buffer(data.deviceId, 'utf8').toString('hex'), 
-		functionId: new Buffer(data.functionId, 'utf8').toString('hex'),
-		functionName: new Buffer(data.functionName, 'utf8').toString('hex')
+HomeFunction.prototype.create = function (data, trs) {
+	trs.recipientId = data.recipientId;
+	trs.asset = {
+		accountId2: new Buffer(data.accountId2, 'utf8').toString('hex'), // Save as hex string
+		deviceId2: new Buffer(data.deviceId2, 'utf8').toString('hex'), 
+		functionId2: new Buffer(data.functionId2, 'utf8').toString('hex'),
+		functionName2: new Buffer(data.functionName2, 'utf8').toString('hex')
 	};
 
-	return trs2;
+	return trs;
 }
 
-HomeFunction.prototype.calculateFee = function (trs2) {
+HomeFunction.prototype.calculateFee = function (trs) {
     return 0; // Free!
 }
 
-HomeFunction.prototype.verify = function (trs2, sender, cb, scope) {
-	/*if (trs2.asset2.deviceId.length > 40) {
+HomeFunction.prototype.verify = function (trs, sender, cb, scope) {
+	/*if (trs.asset.deviceId2.length > 40) {
 		return setImmediate(cb, "Max length of an device id is 20 characters!");
 	}
-	if (trs2.asset2.deviceName.length > 100) {
+	if (trs.asset.deviceName.length > 100) {
 		return setImmediate(cb, "Max length of an device name is 50 characters!");
 	}*/
 
-	setImmediate(cb, null, trs2);
+	setImmediate(cb, null, trs);
 }
 
-HomeFunction.prototype.getBytes = function (trs2) {
-	return new Buffer(trs2.asset2.accountId, 'hex');
-	return new Buffer(trs2.asset2.deviceId, 'hex');
-	return new Buffer(trs2.asset2.functionId, 'hex');
-	return new Buffer(trs2.asset2.functionName, 'hex');
+HomeFunction.prototype.getBytes = function (trs) {
+	return new Buffer(trs.asset.accountId2, 'hex');
+	return new Buffer(trs.asset.deviceId2, 'hex');
+	return new Buffer(trs.asset.functionId2, 'hex');
+	return new Buffer(trs.asset.functionName2, 'hex');
 }
 
-HomeFunction.prototype.apply = function (trs2, sender, cb, scope) {
+HomeFunction.prototype.apply = function (trs, sender, cb, scope) {
     modules.blockchain.accounts.mergeAccountAndGet({
         address: sender.address,
-        balance: -trs2.fee
+        balance: -trs.fee
     }, cb);
 }
 
-HomeFunction.prototype.undo = function (trs2, sender, cb, scope) {
+HomeFunction.prototype.undo = function (trs, sender, cb, scope) {
     modules.blockchain.accounts.undoMerging({
         address: sender.address,
-        balance: -trs2.fee
+        balance: -trs.fee
     }, cb);
 }
 
-HomeFunction.prototype.applyUnconfirmed = function (trs2, sender, cb, scope) {
-    if (sender.u_balance < trs2.fee) {
+HomeFunction.prototype.applyUnconfirmed = function (trs, sender, cb, scope) {
+    if (sender.u_balance < trs.fee) {
         return setImmediate(cb, "Sender doesn't have enough coins");
     }
 
     modules.blockchain.accounts.mergeAccountAndGet({
         address: sender.address,
-        u_balance: -trs2.fee
+        u_balance: -trs.fee
     }, cb);
 }
 
-HomeFunction.prototype.undoUnconfirmed = function (trs2, sender, cb, scope) {
+HomeFunction.prototype.undoUnconfirmed = function (trs, sender, cb, scope) {
     modules.blockchain.accounts.undoMerging({
         address: sender.address,
-        u_balance: -trs2.fee
+        u_balance: -trs.fee
     }, cb);
 }
 
-HomeFunction.prototype.ready = function (trs2, sender, cb, scope) {
+HomeFunction.prototype.ready = function (trs, sender, cb, scope) {
 	setImmediate(cb);
 }
 
-HomeFunction.prototype.save = function (trs2, cb) {
+HomeFunction.prototype.save = function (trs, cb) {
 	modules.api.sql.insert({
 		table: "asset_functions",
 		values: {
-			transactionId: trs2.id,
-			accountId: trs2.asset2.accountId,
-			deviceId: trs2.asset2.deviceId,
-			functionId: trs2.asset2.functionId,
-			functionName: trs2.asset2.functionName
+			transactionId: trs.id,
+			accountId2: trs.asset.accountId2,
+			deviceId2: trs.asset.deviceId2,
+			functionId2: trs.asset.functionId2,
+			functionName2: trs.asset.functionName2
 		}
 	}, cb);
 }
@@ -96,53 +96,53 @@ HomeFunction.prototype.dbRead = function (row) {
 		return null;
 	} else {
 		return {
-			accountId: row.hf_accountId,
-			deviceId: row.hf_deviceId,
-			functionId: row.hf_functionId,
-			functionName: row.hf_functionName
+			accountId2: row.hf_accountId2,
+			deviceId2: row.hf_deviceId2,
+			functionId2: row.hf_functionId2,
+			functionName2: row.hf_functionName2
 		};
 	}
 }
 
-HomeFunction.prototype.normalize = function (asset2, cb) {
-	library.validator.validate(asset2, {
+HomeFunction.prototype.normalize = function (asset, cb) {
+	library.validator.validate(asset, {
 		type: "object", // It is an object
 		properties: {
-			accountId: { // It contains a deviceId property
+			accountId2: { // It contains a deviceId2 property
 				type: "string", // It is a string
 				format: "hex", // It is in a hexadecimal format
 				minLength: 1 // Minimum length of string is 1 character
 			},
-			deviceId: { // It contains a deviceId property
+			deviceId2: { // It contains a deviceId2 property
 				type: "string", // It is a string
 				format: "hex", // It is in a hexadecimal format
 				minLength: 1 // Minimum length of string is 1 character
 			},
-			functionId: { // It contains a functionId property
+			functionId2: { // It contains a functionId2 property
 				type: "string", // It is a string
 				format: "hex", // It is in a hexadecimal format
 				minLength: 1 // Minimum length of string is 1 character
 			},
-			functionName: { // It contains a functionName property
+			functionName2: { // It contains a functionName2 property
 				type: "string", // It is a string
 				format: "hex", // It is in a hexadecimal format
 				minLength: 1 // Minimum length of string is 1 character
 			}
 		},
-		required: ["accountId", "deviceId", "functionId", "functionName"]
+		required: ["accountId2", "deviceId2", "functionId2", "functionName2"]
 	}, cb);
 }
 
 HomeFunction.prototype.onBind = function (_modules) {
 	modules = _modules;
-	modules.logic.transaction.attachAsset2Type(self.type, self);
+	modules.logic.transaction.attachAssetType(self.type, self);
 }
 
 HomeFunction.prototype.putFunction = function (cb, query) {
 	library.validator.validate(query, {
 		type: "object",
 		properties: {
-			accountId: {
+			accountId2: {
 				type: "string",
 				minLength: 1,
 				maxLength: 21
@@ -152,17 +152,17 @@ HomeFunction.prototype.putFunction = function (cb, query) {
 				minLength: 1,
 				maxLength: 100
 			},
-			deviceId: {
+			deviceId2: {
 				type: "string",
 				minLength: 1,
 				maxLength: 20
 			},
-			functionId: {
+			functionId2: {
 				type: "string",
 				minLength: 1,
 				maxLength: 20
 			},
-			functionName: {
+			functionName2: {
 				type: "string",
 				minLength: 1,
 				maxLength: 50
@@ -188,10 +188,10 @@ HomeFunction.prototype.putFunction = function (cb, query) {
 			try {
 				var transaction = library.modules.logic.transaction.create({
 					type: self.type,
-					accountId: query.accountId,
-					deviceId: query.deviceId,
-					functionId: query.functionId,
-					functionName: query.functionName,
+					accountId2: query.accountId2,
+					deviceId2: query.deviceId2,
+					functionId2: query.functionId2,
+					functionName2: query.functionName2,
 					sender: account,
 					keypair: keypair
 				});
@@ -211,12 +211,12 @@ HomeFunction.prototype.getFunctions = function (cb, query) {
     library.validator.validate(query, {
         type: "object",
         properties: {
-            accountId: {
+            accountId2: {
                 type: "string",
                 minLength: 1
             }
         },
-        required: ["accountId"]
+        required: ["accountId2"]
     }, function (err) {
         if (err) {
             return cb(err[0].message);
@@ -227,7 +227,7 @@ HomeFunction.prototype.getFunctions = function (cb, query) {
             table: "transactions",
             alias: "t",
             condition: {
-                accountId: query.accountId,
+                accountId2: query.accountId2,
                 type: self.type
             },
             join: [{
@@ -236,24 +236,24 @@ HomeFunction.prototype.getFunctions = function (cb, query) {
                 alias: "hf",
                 on: {"t.id": "hf.transactionId"}
             }] // The fields have to be in the same order as in the blockchain.json
-        }, ['id', 'type', 'senderId', 'senderPublicKey', 'recipientId', 'amount', 'fee', 'signature', 'blockId', 'transactionId', 'accountId', 'deviceId', 'functionId', 'functionName'], function (err, transactions) {
+        }, ['id', 'type', 'senderId', 'senderPublicKey', 'recipientId', 'amount', 'fee', 'signature', 'blockId', 'transactionId', 'accountId2', 'deviceId2', 'functionId2', 'functionName2'], function (err, transactions) {
             if (err) {
                 return cb(err.toString());
             }
 
-            // Map results to asset2 object
+            // Map results to asset object
             var functions = transactions.map(function (tx) { 
-                tx.asset2 = {
-                	accountId: new Buffer(tx.accountId, 'hex').toString('utf8'),
-                    deviceId: new Buffer(tx.deviceId, 'hex').toString('utf8'),
-                    functionId: new Buffer(tx.functionId, 'hex').toString('utf8'),
-                    functionName: new Buffer(tx.functionName, 'hex').toString('utf8')
+                tx.asset = {
+                	accountId2: new Buffer(tx.accountId2, 'hex').toString('utf8'),
+                    deviceId2: new Buffer(tx.deviceId2, 'hex').toString('utf8'),
+                    functionId2: new Buffer(tx.functionId2, 'hex').toString('utf8'),
+                    functionName2: new Buffer(tx.functionName2, 'hex').toString('utf8')
                 };
 
-                delete tx.accountId;
-                delete tx.deviceId;
-                delete tx.functionId;
-                delete tx.functionName;
+                delete tx.accountId2;
+                delete tx.deviceId2;
+                delete tx.functionId2;
+                delete tx.functionName2;
                 return tx;
             });
 
